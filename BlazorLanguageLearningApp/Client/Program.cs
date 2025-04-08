645a4705a611261167f681e847a179ed886e9fa6
T3tr1s <tomai.kornelmunka@gmail.com>
2025-04-08 19:01:28 +0200

    Change regular Bootstrap to the Blazor.Bootstrap package and fix some of the layout issues that come with it

--- BlazorLanguageLearningApp/Client/Program.cs
+++ BlazorLanguageLearningApp/Client/Program.cs
@@ -10,6 +10,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddSingleton<FolderService>();
 builder.Services.AddSingleton<SetService>();
 builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
