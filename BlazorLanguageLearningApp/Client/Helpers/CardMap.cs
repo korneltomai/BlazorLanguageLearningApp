9db36c9d3a69e52e7c4ffe93deb073f51afd7804
T3tr1s <tomai.kornelmunka@gmail.com>
2025-09-27 15:13:38 +0200

    Refactor file importing and add preview of file content

---
+++ BlazorLanguageLearningApp/Client/Helpers/CardMap.cs
@@ -0,0 +1,15 @@
+﻿using CsvHelper.Configuration;
+using BlazorLanguageLearningApp.Shared;
+
+namespace BlazorLanguageLearningApp.Client.Helpers;
+
+public class CardMap : ClassMap<Card>
+{
+    public CardMap(int termLanguageIndex = 0, int definitionLanguageIndex = 1, int termIndex = 2, int definitionIndex = 3)
+    {
+        Map(m => m.TermLanguage).Index(termLanguageIndex);
+        Map(m => m.DefinitionLanguage).Index(definitionLanguageIndex);
+        Map(m => m.Term).Index(termIndex);
+        Map(m => m.Definition).Index(definitionIndex);
+    }
+}
