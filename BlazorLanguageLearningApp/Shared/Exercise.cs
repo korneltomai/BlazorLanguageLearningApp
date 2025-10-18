99db93cc35af283d9b67c2de573b1307f25a6fd3
T3tr1s <tomai.kornelmunka@gmail.com>
2025-10-18 14:50:03 +0200

    Add components for 3 types of exercises

---
+++ BlazorLanguageLearningApp/Shared/Exercise.cs
@@ -0,0 +1,36 @@
+﻿namespace BlazorLanguageLearningApp.Shared;
+
+public class Exercise
+{
+    public ExerciseType Type { get; set; }
+    public ExerciseEntry Question { get; set; }
+    public List<ExerciseEntry> PossibleAnswers { get; set; }
+    public ExerciseEntry UserAnswer { get; set; } = new();
+
+    public Exercise(ExerciseType type, ExerciseEntry question, List<ExerciseEntry> possibleAnswers)
+    {
+        Type = type;
+        Question = question;
+        PossibleAnswers = possibleAnswers;
+    }
+}
+
+public enum ExerciseType { TrueOrFalse, Selection, TypeIn }
+
+public class ExerciseEntry 
+{
+    public string Expression { get; set; }
+    public string Language { get; set; }
+
+    public ExerciseEntry()
+    {
+        Expression = "";
+        Language = "";
+    }
+
+    public ExerciseEntry(string expression, string language)
+    {
+        Expression = expression;
+        Language = language;
+    }
+}
