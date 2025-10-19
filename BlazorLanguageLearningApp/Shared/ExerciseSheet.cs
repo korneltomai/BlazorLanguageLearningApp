b955a5a42d838f01447bf5925c9d675973fc4b11
T3tr1s <tomai.kornelmunka@gmail.com>
2025-10-19 20:41:36 +0200

    Add basic layout for exercises page

---
+++ BlazorLanguageLearningApp/Shared/ExerciseSheet.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorLanguageLearningApp.Shared;
+
+public class ExerciseSheet
+{
+    public List<Exercise> Exercises { get; set; }
+
+    public ExerciseSheet() 
+    {
+        Exercises = new List<Exercise>();
+    }
+
+    public ExerciseSheet(List<Exercise> exercises)
+    {
+        Exercises = exercises;
+    }
+}
